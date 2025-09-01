89b8fcd4af020780d61ae21462a05774a565cd35
geofrey <devongeoffreymaina@gmail.com>
2025-09-01 15:42:10 +0300

    Configured postgres database

--- willamabackend/settings.py
+++ willamabackend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import dj_database_url
 
 
 env = environ.Env()
@@ -92,11 +93,19 @@ WSGI_APPLICATION = 'willamabackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600,
+        ssl_require=True,  # important for Supabase
+    )
 }
 
 
